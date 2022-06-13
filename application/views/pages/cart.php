d25c0daab9d56bc5d10da9dfaeeb54bd7dd5e770
collins Tuti <75304270+TUTI254@users.noreply.github.com>
2022-06-13 17:33:22 +0300

    Update cart.php

--- application/views/pages/cart.php
+++ application/views/pages/cart.php
@@ -9,9 +9,10 @@
                         </div>
                     </div>
 					<?php if($this->cart->total() != 0):?>
-						<form action="">
-							<?php foreach($cartItems  as$cart):?>
-								<div class="d-flex flex-row justify-content-between align-items-center p-2 bg-white mt-4 px-3 rounded">
+
+						<form action="" id="loadcart">
+							<?php foreach($cartItems as $cart):?>
+								<div class="d-flex flex-row justify-content-between align-items-center p-2 bg-white mt-4 px-3 rounded" id="cartdata">
 									<div class="mr-1"><img class="rounded" src="<?php echo base_url(''.$cart['image']);?>" width="80"></div>
 									<div class="d-flex flex-column align-items-left product-details"><span class="font-weight-bold font-size-8px"><?= $cart['name']?></span>
 									</div>
@@ -21,15 +22,19 @@
 									<div>
 										<h5 class="text-grey ml-2 mr-1"><?= 'KES '.number_format($cart['price']); ?></h5>
 									</div>
-									<div class="d-flex align-items-center"><i class="fa fa-trash mb-1 text-danger ml-2" onclick="removeFromCart(<?php echo' \''.$cart['rowid'].'\''?>)"></i>
+									<div class="d-flex align-items-center">
+									<?php echo '<i onclick="removeFromCart(\''.$cart['rowid'].'\')" class="fa fa-trash remove"></i>'?>
 									</div>
-								</div>
-									<!-- <div class="d-flex flex-row align-items-center mt-3 p-2 bg-white rounded"><input type="text" class="form-control border-0 gift-card" placeholder="discount code/gift card"><button class="btn btn-outline-warning btn-sm ml-2" type="button">Apply</button></div> -->
-									<div class="d-flex flex-row align-items-center mt-3">
-										<a href="<?=base_url('checkout');?>" class="red_button text-white p-3 ml-3">Proceed to checkout</a>
-										<a href="<?=base_url('shop');?>" class="red_button text-white p-3 ml-3">continue shopping</a>
-									</div>
+
+								</div> 
+								<hr>
+								<!-- <div class="d-flex flex-row align-items-center mt-3 p-2 bg-white rounded"><input type="text" class="form-control border-0 gift-card" placeholder="discount code/gift card"><button class="btn btn-outline-warning btn-sm ml-2" type="button">Apply</button></div> -->
 								<?php endforeach ;?>
+									<div class="d-flex flex-row align-items-center mt-3">
+										<a href="<?=base_url('shop');?>" class="red_button text-white p-3 ml-3">continue shopping</a>
+										<a href="<?=base_url('checkout');?>" class="red_button text-white p-3 ml-3">Proceed to checkout</a>
+									
+									</div>
 						</form>
                     
 					<?php else: ?>
