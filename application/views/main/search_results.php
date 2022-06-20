d18db7e98994d27516143d9e533c1d3ca01244b3
collins Tuti <75304270+TUTI254@users.noreply.github.com>
2022-06-20 16:05:16 +0300

    Update search_results.php

--- application/views/main/search_results.php
+++ application/views/main/search_results.php
@@ -6,15 +6,15 @@
 								<div class="product-item women">
 									<div class="product product_filter">
 										<div class="product_image">
-											<img src="<?php echo base_url(''.$row['image']);?>" alt="">
+											<img src="<?php echo base_url(''.$row->image);?>" alt="">
 										</div>
 										<div class="favorite"></div>
 										<div class="product_info">
-											<h6 class="product_name"><a href="single.html"><?php echo $row["name"]; ?></a></h6>
-											<div class="product_price"><?php echo 'KES '.$row["price"]; ?></div>
+											<h6 class="product_name"><a href="single.html"><?php echo $row->name; ?></a></h6>
+											<div class="product_price"><?php echo 'KES '.$row->price; ?></div>
 										</div>
 									</div>
-									<div class="red_button add_to_cart_button"><a href="<?php echo base_url('products/addToCart/'.$row['id']); ?>">add to order</a></div>
+									<div class="red_button add_to_cart_button"><a href="<?php echo base_url('products/addToCart/'.$row->id); ?>">add to order</a></div>
 								</div>
 							<?php  endforeach;?>
 						</div>
